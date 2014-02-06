aa20f8de4940853eea9d57a9183a4d869a44abc6
rho <ryan@theoleschool.com>
2014-02-05 23:59:25 -0600

    updating for mongo on heroku

--- model/db.js
+++ model/db.js
@@ -10,4 +10,4 @@ var noaaSchema = mongoose.Schema({
 
 module.exports = mongoose.model('NoaaData', noaaSchema);
 
-mongoose.connect('mongodb://127.0.0.1/noaa');
+mongoose.connect('mongodb://heroku_app21958260:6p4ho4pf059p2tklur2c2369va@ds027729.mongolab.com:27729/heroku_app21958260');
