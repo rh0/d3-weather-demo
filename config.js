6e254e7d7e08d876d8dd19179ff7e2d167b506bf
rho <ryan@theoleschool.com>
2014-02-10 20:21:37 -0600

    opening up config for heroku

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * Config file to store sensitive mongo db connection information.
+ * This should not be included in your repository!
+ */
+var config = {}
+
+config.dbUser = "heroku_app21958260";
+config.dbPw = "6p4ho4pf059p2tklur2c2369va";
+config.dbPath = "ds027729.mongolab.com:27729/heroku_app21958260";
+
+module.exports = config;
